c8100eecf78dd6a2fd54a748e3ba4e876d6968c2
D4RK PH1N1X <71398334+D4RK-PH1N1X@users.noreply.github.com>
2020-09-17 16:18:58 +0600

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,168 @@
+#!/data/data/com.termux/files/usr/bash
+clear
+echo $red 
+ echo ""-----------------------------------------------------------------------------""|lolcat
+echo " D4RK PH1N1X * Basic all packages in one script "|lolcat
+echo "---------------------------------------------------------------------------------"|lolcat
+
+echo
+
+echo  "★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★"|lolcat
+echo   "
+
+
+▓█████▄  ▄▄▄       ██▀███   ██ ▄█▀
+▒██▀ ██▌▒████▄    ▓██ ▒ ██▒ ██▄█▒ 
+░██   █▌▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░ 
+░▓█▄   ▌░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄ 
+░▒████▓  ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄
+ ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒
+ ░ ▒  ▒   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░
+ ░ ░  ░   ░   ▒     ░░   ░ ░ ░░ ░ 
+   ░          ░  ░   ░     ░  ░   
+ ░                                
+
+
+
+ ██▓███   ██░ ██  ██▓ ███▄    █  ██▓▒██   ██▒
+▓██░  ██▒▓██░ ██▒▓██▒ ██ ▀█   █ ▓██▒▒▒ █ █ ▒░
+▓██░ ██▓▒▒██▀▀██░▒██▒▓██  ▀█ ██▒▒██▒░░  █   ░
+▒██▄█▓▒ ▒░▓█ ░██ ░██░▓██▒  ▐▌██▒░██░ ░ █ █ ▒ 
+▒██▒ ░  ░░▓█▒░██▓░██░▒██░   ▓██░░██░▒██▒ ▒██▒
+▒▓▒░ ░  ░ ▒ ░░▒░▒░▓  ░ ▒░   ▒ ▒ ░▓  ▒▒ ░ ░▓ ░
+░▒ ░      ▒ ░▒░ ░ ▒ ░░ ░░   ░ ▒░ ▒ ░░░   ░▒ ░
+░░        ░  ░░ ░ ▒ ░   ░   ░ ░  ▒ ░ ░    ░  
+          ░  ░  ░ ░           ░  ░   ░    ░  
+                                             
+
+"|lolcat -a -d 10               
+ echo   " This Script is Made by D4RK PH1N1X "|lolcat -a -d 30                                                                    
+echo   "#################################################################################"|lolcat
+
+echo
+echo
+echo
+echo
+echo "Are You Ready?"|lolcat -a -d 30
+echo "START!!!"|lolcat
+echo "".................................................................................""|lolcat
+echo "".................................................................................""|lolcat
+echo  ""↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓""|lolcat
+sleep 5
+echo "."
+
+
+apt update && apt upgrade -y 
+#update in progress
+pkg install python -y           #installation......          
+pkg install git -y
+pkg install python2 -y
+pkg install grep -y
+pkg install wget -y
+pkg install ruby -y
+pkg install openssh -y
+pkg install vim -y
+pkg install root-repo -y
+pkg install x11-repo -y
+pkg install unstable-repo -y
+pkg install bash -y
+pkg install zsh -y
+pip install lolcat -y
+pkg install mpv -y
+pkg install ncurses-utils -y
+pkg install clang -y
+pkg install netcat -y
+pkg install cmatrix -y
+pkg install sl -y
+pkg install libcaca -y
+termux-setup-storage
+pip install --upgrade pip
+pip2 install --upgrade pip
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pkg install python2-dev -y
+pkg install python3 -y
+pkg install java -y
+pkg install fish -y
+pkg install ruby -y
+pkg install help -y
+pkg install host -y
+pkg install php -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install nano -y
+pkg install w3m -y
+pkg install havij -y
+pkg install hydra -y
+pkg install vim -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install lolcat -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install google -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wireshark -y
+pkg install wgetrc -y 
+pkg install wcalc -y
+pkg install bmon -y
+pkg install vpn -y
+pkg install unrar -y
+pkg install toilet -y
+pkg install proot -y
+pkg install net-tools -y
+pkg install golang -y
+pkg install chroot -y
+termux-chroot -y
+pkg install macchanger -y
+pkg install openssl -y
+pkg install cmatrix -y
+pkg install openssh -y
+pkg install wireshark -y
+pkg install macchanger -y
+clear
+
+echo   "                                                                                
+
+▓█████▄  ▄▄▄       ██▀███   ██ ▄█▀
+▒██▀ ██▌▒████▄    ▓██ ▒ ██▒ ██▄█▒
+░██   █▌▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░
+░▓█▄   ▌░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄
+░▒████▓  ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄
+ ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒                                                       ░ ▒  ▒   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░
+ ░ ░  ░   ░   ▒     ░░   ░ ░ ░░ ░                                                          ░          ░  ░   ░     ░  ░
+ ░
+
+
+
+ ██▓███   ██░ ██  ██▓ ███▄    █  ██▓▒██   ██▒
+▓██░  ██▒▓██░ ██▒▓██▒ ██ ▀█   █ ▓██▒▒▒ █ █ ▒░
+▓██░ ██▓▒▒██▀▀██░▒██▒▓██  ▀█ ██▒▒██▒░░  █   ░
+▒██▄█▓▒ ▒░▓█ ░██ ░██░▓██▒  ▐▌██▒░██░ ░ █ █ ▒
+▒██▒ ░  ░░▓█▒░██▓░██░▒██░   ▓██░░██░▒██▒ ▒██▒
+▒▓▒░ ░  ░ ▒ ░░▒░▒░▓  ░ ▒░   ▒ ▒ ░▓  ▒▒ ░ ░▓ ░
+░▒ ░      ▒ ░▒░ ░ ▒ ░░ ░░   ░ ▒░ ▒ ░░░   ░▒ ░
+░░        ░  ░░ ░ ▒ ░   ░   ░ ░  ▒ ░ ░    ░                                                       ░  ░  ░ ░           ░  ░   ░    ░
+
+
+"
+
+
+
+echo " If Your Network's Connected "|lolcat
+echo " YOUR PACKAGES Have Been INSTALLED SUCCESSFULLY !!! "|lolcat
+echo "Thanks!"|lolcat -a -d 30
+echo "Mr. SS7"|lolcat -a -d 30
+echo "D4RK PH1N1X"|lolcat -a -d 30
+sleep 20
+clear
+exit
+
